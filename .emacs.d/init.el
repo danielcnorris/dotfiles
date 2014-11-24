051061f379c4fc8b0445aea5581c6d5bb11a4197
danielnorris <daniel@quantifiedlabs.com>
2014-11-24 13:30:46 -0500

    now uses melpa to get evil and solarized

---
+++ .emacs.d/init.el
@@ -0,0 +1,40 @@
+; Add MELPA packages
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.milkbox.net/packages/") t)
+
+(defun require-package (package &optional min-version no-refresh)
+  "Install given PACKAGE, optionally requiring MIN-VERSION.
+If NO-REFRESH is non-nil, the available package lists will not be
+re-downloaded in order to locate PACKAGE."
+  (if (package-installed-p package min-version)
+      t
+    (if (or (assoc package package-archive-contents) no-refresh)
+        (package-install package)
+      (progn
+        (package-refresh-contents)
+        (require-package package min-version t)))))
+
+(package-initialize)
+
+; Use evil mode
+(require-package 'evil)
+
+(setq evil-search-module 'evil-search
+      evil-want-C-u-scroll t
+      evil-want-C-w-in-emacs-state t)
+
+(require 'evil)
+(evil-mode t)
+
+; Load solarized theme
+(require-package 'color-theme-solarized)
+; (add-to-list 'custom-theme-load-path "emacs-color-theme-solarized")
+(load-theme 'solarized-dark t)
+
+; Org mode setup
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cb" 'org-iswitchb)
+(display-color-cells (selected-frame))
