e9fff7a11868b5e76b986bc6046d5fb83ddbb4d3
Daniel Norris <danielcnorris@Daniels-MacBook-Air.local>
2014-12-12 00:22:12 -0500

    added smex and ido additions

--- .emacs.d/lisp/init-ido.el
+++ .emacs.d/lisp/init-ido.el
@@ -3,6 +3,11 @@
 ;; Configures Ido mode
 ;;; Code:
 
+;;(require-package 'fix-ido)
+(require-package 'ido-hacks)
+(require-package 'ido-vertical-mode)
+(require-package 'smex)
+
 (require 'ido)
 (ido-mode t)
 (ido-everywhere t)
@@ -10,6 +15,15 @@
 (setq ido-use-filename-at-point nil)
 (setq ido-auto-merge-work-directories-length 0)
 (setq ido-use-virtual-buffers t)
+
+(require 'ido-hacks)
+(ido-vertical-mode t)
+;;(fix-ido-mode t)
+
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
 (provide 'init-ido)
 ;;; init-ido.el ends here
