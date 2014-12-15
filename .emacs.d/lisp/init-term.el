10b03c623d2df23a56cb008a9b0bdb9be8f3d4ed
Daniel Norris <danielcnorris@mgail.com>
2014-12-15 11:50:21 -0500

    added multi-term shortcuts

--- .emacs.d/lisp/init-term.el
+++ .emacs.d/lisp/init-term.el
@@ -9,5 +9,18 @@
 
 (global-set-key (kbd "\C-cd") 'multi-term)
 
+;; Allow navigation between terminal buffers
+(add-hook 'term-mode-hook
+          (lambda ()
+              (add-to-list 'term-bind-key-alist '("M-["
+                                                  . multi-term-prev))
+              (add-to-list 'term-bind-key-alist '("M-]"
+                                                   . multi-term-next))))
+
+;; Enable proper pasting into terminal buffers
+(add-hook 'term-mode-hook
+          (lambda ()
+              (define-key term-raw-map (kbd "\C-y") 'term-paste)))
+
 (provide 'init-term)
 ;;; init-term.el ends here
