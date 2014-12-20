ecfe206640052a219eede8e4c2c66b375c57ed81
Daniel Norris <danielcnorris@daniels-air.nyc.rr.com>
2014-12-20 13:12:53 -0500

    Added project narrowing functions

--- .emacs.d/lisp/init-org.el
+++ .emacs.d/lisp/init-org.el
@@ -383,6 +383,219 @@ Callers of this function already widen the buffer view."
                 (or subtree-end (point-max)))
             next-headline)))
 
+;; Enable narrowing to subtree
+(defun dcn/org-todo (arg)
+    (interactive "p")
+    (if (equal arg 4)
+            (save-restriction
+                (dcn/narrow-to-org-subtree)
+                (org-show-todo-tree nil))
+        (dcn/narrow-to-org-subtree)
+        (org-show-todo-tree nil)))
+
+(defun dcn/widen ()
+    (interactive)
+    (if (equal major-mode 'org-agenda-mode)
+            (progn
+                (org-agenda-remove-restriction-lock)
+                (when org-agenda-sticky
+                    (org-agenda-redo)))
+        (widen)))
+
+(add-hook 'org-agenda-mode-hook
+          '(lambda ()
+               (org-defkey org-agenda-mode-map "W"
+                (lambda ()
+                    (interactive)
+                    (setq dcn/hide-scheduled-and-waiting-next-tasks t)
+                    (dcn/widen))))
+          'append)
+
+
+;; Limit agenda to subtree
+(defun dcn/set-agenda-restriction-lock (arg)
+  "Set restriction lock to current task subtree or file if ARG."
+  (interactive "p")
+  (let* ((pom (dcn/get-pom-from-agenda-restriction-or-point))
+          (tags (org-with-point-at pom (org-get-tags-at))))
+         (let ((restriction-type (if (equal arg 4) 'file 'subtree)))
+           (save-restriction
+             (cond
+               ((and (equal major-mode 'org-agenda-mode) pom)
+                (org-with-point-at pom
+                  (org-agenda-set-restriction-lock restriction-type))
+                (org-agenda-redo))
+               ((and (equal major-mode 'org-mode)
+                     (org-before-first-heading-p))
+                (org-agenda-set-restriction-lock 'file))
+               (pom
+                 (org-with-point-at pom
+                                    (org-agenda-set-restriction-lock
+                                      restriction-type))))))))
+
+(add-hook 'org-agenda-mode-hook
+          '(lambda ()
+             (org-defkey org-agenda-mode-map "\C-c\C-x<"
+                         'dcn/set-agenda-restriction-lock))
+          'append)
+
+;; What is this for?
+;; Follow mode?
+(defun dcn/restrict-to-file-or-follow (arg)
+    "Set agenda restrictionto 'file or with ARG follow mode."
+    (interactive "p")
+    (if (equal arg 4)
+            (org-agenda-follow-mode)
+        (widen)
+        (dcn/set-agenda-restriction-lock 4)
+        (org-agenda-redo)
+        (beginning-of-buffer)))
+
+(add-hook 'org-agenda-mode-hook
+          '(lambda ()
+               (org-defkey org-agenda-mode-map "F"
+                           'dcn/restrict-to-file-or-follow))
+          'append)
+
+(defun dcn/narrow-to-org-subtree ()
+    (widen)
+    (org-narrow-to-subtree)
+    (save-restriction
+        (org-agenda-set-restriction-lock)))
+
+(defun dcn/narrow-to-subtree ()
+    (interactive)
+    (if (equal major-mode 'org-agenda-mode)
+            (progn
+                (org-with-point-at
+                        (org-get-at-bol 'org-hd-marker)
+                    (dcn/narrow-to-org-subtree))
+                (when org-agenda-sticky
+                    (org-agenda-redo)))
+    (dcn/narrow-to-org-subtree)))
+
+(add-hook 'org-agenda-mode-hook
+          '(lambda ()
+               (org-defkey org-agenda-mode-map "N"
+                           'dcn/narrow-to-subtree))
+          'append)
+
+(defun dcn/narrow-up-one-org-level ()
+     (widen)
+     (save-excursion
+         (outline-up-heading 1 'invisible-ok)
+         (dcn/narrow-to-org-subtree)))
+
+;; What does it do?
+(defun dcn/get-pom-from-agenda-restriction-or-point ()
+    (or (and (marker-position org-agenda-restrict-begin)
+             org-agenda-restrict-begin)
+        (org-get-at-bol 'org-hd-marker)
+        (and (equal major-mode 'org-mode) (point))
+        org-clock-marker))
+
+(defun dcn/narrow-up-one-level ()
+    (interactive)
+    (if (equal major-mode 'org-agenda-mode)
+            (progn
+                (org-with-point-at
+                        (dcn/get-pom-from-agenda-restriction-or-point)
+                    (dcn/narrow-up-one-org-level))
+                (org-agenda-redo))
+        (dcn/narrow-up-one-org-level)))
+
+(add-hook 'org-agenda-mode-hook
+          '(lambda ()
+               (org-defkey org-agenda-mode-map "U"
+                           'dcn/narrow-up-one-level))
+          'append)
+
+(defun dcn/narrow-to-org-project ()
+    (widen)
+    (save-excursion
+        (dcn/find-project-task)
+        (dcn/narrow-to-org-subtree)))
+
+(defun dcn/narrow-to-project ()
+    (interactive)
+    (if (equal major-mode 'org-agenda-mode)
+            (progn
+                (org-with-point-at
+                        (dcn/get-pom-from-agenda-restriction-or-point)
+                    (dcn/narrow-to-org-project)
+                    (save-excursion
+                        (dcn/find-project-task)
+                        (org-agenda-set-restriction-lock)))
+                (org-agenda-redo)
+                (beginning-of-buffer))
+        (dcn/narrow-to-org-project)
+        (save-restriction
+            (org-agenda-set-restriction-lock))))
+
+(add-hook 'org-agenda-mode-hook
+          '(lambda ()
+                    (org-defkey org-agenda-mode-map "P"
+                                'dcn/narrow-to-project))
+          'append)
+
+(defvar dcn/project-list nil)
+
+(defun dcn/view-next-project ()
+    (interactive)
+    (let (num-project-left current-project)
+        (unless (marker-position org-agenda-restrict-begin)
+            (goto-char (point-min))
+            ;; Clear existing markers on list
+            (while dcn/project-list
+                (set-marker (pop dcn/project-list) nil))
+            (re-search-forward "New Tasks")
+            (forward-visible-line 1))
+
+        ;; Build a new project marker list
+        (unless dcn/project-list
+            (while (< (point) (point-max))
+                (while (and (< (point) (point-max))
+                            (or (not (org-get-at-bol 'org-hd-marker))
+                                (org-with-point-at
+                                        (org-get-at-bol
+                                         'org-hd-marker)
+                                    (or (not (dcn/is-project-p))
+                                        (dcn/is-project-subtree-p)))))
+                    (forward-visible-line 1))
+                (when (< (point) (point-max))
+                    (add-to-list 'dcn/project-list
+                                 (copy-marker
+                                  (org-get-at-bol 'org-hd-marker))
+                                 'append))
+                (forward-visible-line 1)))
+
+        ;; Pop off first marker on list and display
+        (setq current-project (pop dcn/project-list))
+        (when current-project
+            (org-with-point-at current-project
+                (setq dcn/hide-scheduled-and-waiting-next-tasks nil)
+                (dcn/narrow-to-project))
+            ;; Remove marker
+            (setq current-project nil)
+            (org-agenda-redo)
+            (beginning-of-buffer)
+            (setq num-projects-left (length dcn/project-list))
+            (if (> num-projects-left 0)
+                    (message "%s projects left to view"
+                             num-projects-left)
+                (beginning-of-buffer)
+                (setq dcn/hide-scheduled-and-waiting-next-tasks t)
+                (error "All projects viewed.")))))
+
+(add-hook 'org-agenda-mode-hook
+          '(lambda ()
+               (org-defkey org-agenda-mode-map "V"
+                           'dcn/view-next-project))
+          'append)
+
+;; Show next headline
+(setq org-show-entry-below (quote ((default))))
+
 ;; Create an inactive timestamp with new header
 (defvar dcn/insert-inactive-timestamp t)
 
