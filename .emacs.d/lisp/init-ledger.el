1180a2d772c46ff80fc3654956a33ffcf5490411
Daniel Norris <danielcnorris@mgail.com>
2014-12-26 19:33:33 -0500

    Added ledger schedule file

--- .emacs.d/lisp/init-ledger.el
+++ .emacs.d/lisp/init-ledger.el
@@ -5,6 +5,8 @@
 (require-package 'ledger-mode)
 (autoload 'ledger-mode "ledger-mode" "A major mode for Ledger" t)
 (add-to-list 'auto-mode-alist '("\\.ledger$" . ledger-mode))
+(setq ledger-schedule-file (concat dcn/ledger-directory
+                                   "ledger-schedule.ledger"))
 
 (provide 'init-ledger)
 ;;; init-ledger.el ends here
