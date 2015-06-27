443cc05ab92613530dbc4a3f75e99e1a07d07034
Daniel Norris <dnorris@palantir.com>
2015-06-26 23:13:31 -0400

    actually added ess

---
+++ .emacs.d/lisp/init-ess.el
@@ -0,0 +1,19 @@
+;;; init-ess.el --- configure ESS
+;;; Commentary:
+;; Setup for data analysis
+;;; Code:
+
+(require-package 'ess)
+(require 'ess-site)
+(setq ess-eval-visibly nil)
+(setq ess-ask-for-ess-directory nil)
+(setq auto-mode-alist
+      (append '(("\\.r" . R-mode))
+              auto-mode-alist))
+
+(add-hook 'ess-mode-hook
+          (lambda () (flycheck-mode -1)))
+(add-hook 'inferior-ess-mode-hook
+          (lambda () (linum-mode -1)))
+(provide 'init-ess)
+;; init-ess.el ends here
