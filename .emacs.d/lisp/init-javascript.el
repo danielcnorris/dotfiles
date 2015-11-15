d4fe8548420cadee8146006c80de30d46438f290
Daniel Norris <dnorris@palantir.com>
2015-11-15 18:06:56 -0500

    tried to modify js for jsx

--- .emacs.d/lisp/init-javascript.el
+++ .emacs.d/lisp/init-javascript.el
@@ -5,5 +5,42 @@
 (require-package 'coffee-mode)
 (custom-set-variables '(coffee-tab-width 4))
 
+(require-package 'js2-mode)
+(require-package 'web-mode)
+(require-package 'json-mode)
+(require-package 'exec-path-from-shell)
+
+
+;; use web-mode for .jsx files
+(add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
+
+;; disable jshint since we prefer eslint checking
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(javascript-jshint)))
+
+;; use eslint with web-mode for jsx files
+(flycheck-add-mode 'javascript-eslint 'web-mode)
+
+;; disable json-jsonlist checking for json files
+(setq-default flycheck-disabled-checkers
+  (append flycheck-disabled-checkers
+    '(json-jsonlist)))
+
+;; https://github.com/purcell/exec-path-from-shell
+;; only need exec-path-from-shell on OSX
+;; this hopefully sets up path and other vars better
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+;; adjust indents for web-mode to 2 spaces
+(defun my-web-mode-hook ()
+  "Hooks for Web mode. Adjust indents"
+  ;;; http://web-mode.org/
+  (setq web-mode-markup-indent-offset 2)
+  (setq web-mode-css-indent-offset 2)
+  (setq web-mode-code-indent-offset 2))
+(add-hook 'web-mode-hook  'my-web-mode-hook)
+
 (provide 'init-javascript)
 ;;; init-javascript.el ends here
