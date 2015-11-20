642398dde96c1ac5352a488d18225bd6a2e34acf
Daniel Norris <dnorris@palantir.com>
2015-11-20 15:21:24 -0500

    updated javascript

--- .emacs.d/lisp/init-javascript.el
+++ .emacs.d/lisp/init-javascript.el
@@ -14,6 +14,12 @@
 ;; use web-mode for .jsx files
 (add-to-list 'auto-mode-alist '("\\.jsx$" . web-mode))
 
+(defadvice web-mode-highlight-part (around tweak-jsx activate)
+  (if (equal web-mode-content-type "jsx")
+      (let ((web-mode-enable-part-face nil))
+        ad-do-it)
+    ad-do-it))
+
 ;; disable jshint since we prefer eslint checking
 (setq-default flycheck-disabled-checkers
   (append flycheck-disabled-checkers
@@ -33,14 +39,14 @@
 (when (memq window-system '(mac ns))
   (exec-path-from-shell-initialize))
 
-;; adjust indents for web-mode to 2 spaces
-(defun my-web-mode-hook ()
-  "Hooks for Web mode. Adjust indents"
-  ;;; http://web-mode.org/
-  (setq web-mode-markup-indent-offset 2)
-  (setq web-mode-css-indent-offset 2)
-  (setq web-mode-code-indent-offset 2))
-(add-hook 'web-mode-hook  'my-web-mode-hook)
+(setq javascript-indent-level 2)
+(setq js-indent-level 2)
+(setq coffee-tab-width 2)
+(setq js2-basic-offset 2)js-indent-level
+(setq web-mode-markup-indent-offset 2)
+(setq web-mode-css-indent-offset 2)
+(setq web-mode-code-indent-offset 2)
+(setq css-indent-offset 2)
 
 (provide 'init-javascript)
 ;;; init-javascript.el ends here
