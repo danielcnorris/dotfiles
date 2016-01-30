4092784116a4b0c7ee0c9780773e13457bd6dd4f
Daniel Norris <dnorris@palantir.com>
2016-01-30 09:45:40 -0500

    now use sym links for dotfiles

--- install.sh
+++ install.sh
@@ -1,38 +1,33 @@
 #!/bin/bash
 
-function install() {
-    rsync --exclude ".git/"  \
-          --exclude ".DS_Store" \
-          --exclude "install.sh" \
-          --exclude "README.md" \
-          --exclude "LICENSE" \
-          --exclude "scripts/" \
-          -avh --no-perms . $HOME
+dir=$(pwd)
+declare -a files=("bash_profile"
+                  "bashrc"
+                  "emacs.d"
+                  "ghci"
+                  "gitconfig"
+                  "tmux.conf"
+                  "vim"
+                  "vimoutlinerrc"
+                  "vimrc")
 
-    if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
-        git clone https://github.com/gmarik/Vundle.vim.git \
-            $HOME/.vim/bundle/Vundle.vim
-    fi
+for file in "${files[@]}"
+do
+    unlink $HOME/.$file
+    ln -s $dir/$file $HOME/.$file
+done
 
-    echo "" >> $HOME/.gitconfig
-    echo "[user]" >> $HOME/.gitconfig
-    echo "    name = $GITUSER" >> $HOME/.gitconfig
-    echo "    email = $GITEMAIL" >> $HOME/.gitconfig
-
-    vim +PluginInstall +qall
-    source $HOME/.bash_profile
-
-    cp "$SECRETS/secrets.el" $HOME/.emacs.d/lisp/
-}
-
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    install
-else
-    read -p "This operation may override files in your home directory. Proceed? [y/n]"
-    echo ""
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        install
-    fi
+if [ ! -d $HOME/.vim/bundle/Vundle.vim ]; then
+    git clone https://github.com/gmarik/Vundle.vim.git \
+        $HOME/.vim/bundle/Vundle.vim
 fi
 
-unset install
+echo "" >> $HOME/.gitconfig
+echo "[user]" >> $HOME/.gitconfig
+echo "    name = $GITUSER" >> $HOME/.gitconfig
+echo "    email = $GITEMAIL" >> $HOME/.gitconfig
+
+vim +PluginInstall +qall
+source $HOME/.bash_profile
+
+cp "$SECRETS/secrets.el" $HOME/.emacs.d/lisp/
