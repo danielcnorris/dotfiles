341c2ab847007efd18935e11888c67fabe7ae38c
Daniel Norris <danielcnorris@gmail.com>
2017-10-09 20:12:22 -0700

    Increased ST font size.

--- abs/dwm/config.h
+++ abs/dwm/config.h
@@ -56,8 +56,8 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", normbgcolor, "-nf", normfgcolor, "-sb", selbgcolor, "-sf", selfgcolor, NULL };
-static const char *termcmd[]  = { "st", NULL };
 /* DCN commands. */
+static const char *termcmd[]  = { "st", "-f", "Inconsolata:size=13", NULL };
 static const char *lockcmd[]  = { "slock", NULL };
 static const char *internetcmd[]  = { "chromium", NULL };
 static const char *screenshotcmd[]  = { "scrot", NULL };
