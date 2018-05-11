5b84678984caa28eae154403cf1af8b1c108e703
Daniel Norris <danielcnorris@gmail.com>
2018-05-11 15:30:07 -0700

    Add macos install script.

--- scripts/install_macos.sh
+++ scripts/install_macos.sh
@@ -1,8 +1,35 @@
-# TODO Install homebrew and apps
-# TODO Install homebrew cask and aps (chrome, iterm, anki)
-# TODO Install python packages
-# TODO Install fd (brew)
-# TODO Install reattach to user namespace
-# TODO Install cmake
-# TODO Install hub
-# TODO Install vim override, with Lua and with python3.
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+binaries=(
+  go
+  mysql
+  node
+  postgresql
+  python
+  reattach-to-user-namespace
+  ripgrep
+  stack
+  tmux
+)
+
+brew install ${binaries[@]}
+
+brew services start --all
+
+brew install vim --override-system-vi --with-lua --with-python3
+
+brew tap caskroom/cask
+
+apps=(
+  docker
+  google-chrome
+  iterm2
+)
+
+brew cask install --appdir="/Applications" ${apps[@]}
+
+# https://help.github.com/enterprise/2.13/user/articles/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent/
+ssh-keygen -t rsa -b 4096 -C "danielcnorris@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add -K ~/.ssh/id_rsa
