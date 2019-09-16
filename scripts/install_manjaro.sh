aa255ba379d73cbd11e7ef34acde372f184962fd
Daniel Norris <danielcnorris@gmail.com>
2019-09-16 14:22:29 -0700

    Additional XFCE terminal suggestions.

--- scripts/install_manjaro.sh
+++ scripts/install_manjaro.sh
@@ -44,6 +44,8 @@ sudo systemctl enable --now docker.service
 # TODO Map caps to ctrl.
 # XFCE Terminal > Edit > Preferences > Appearance > Opacity to 100%.
 # XFCE Terminal > Edit > Preferences > Colors > Dark Pastel
+# XFCE Terminal Preferences > Compatability > Backspace generates ASCII DEL
+# XFCE Terminal Preferences > Advanced > Disable all menu access keys
 # If you want to change the default browser:
 # https://www.reddit.com/r/ManjaroLinux/comments/7k8xi6/how_to_set_the_default_browser/
 # Enable Emacs keybindings in GTK: https://wiki.archlinux.org/index.php/GTK#Keyboard_shortcuts
