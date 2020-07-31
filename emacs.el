618cd20e2820fef1376bf7504d8fd5c1fb659529
Daniel Norris <danielcnorris@gmail.com>
2020-07-31 12:42:47 -0700

    Org updates.

--- emacs.el
+++ emacs.el
@@ -12,25 +12,22 @@
 
 ;; Navigation
 ;; Search
-;; Knowldge organization
-;; Writing
 
 (setq org-special-ctrl-a/e t
       org-special-ctrl-o t
       org-use-speed-commands t
+      org-reverse-note-order t
       org-enforce-todo-dependencies t
       org-enforce-todo-checkbox-dependencies t
       org-insert-heading-respect-content t
       org-log-into-drawer t
       org-todo-keywords
       '((sequence
-	 "TODO(t)"
 	 "NEXT(n!)"
+	 "TODO(t)"
 	 "WAITING(w@)"
-	 "FUTURE(f)"
 	 "|"
-	 "DONE(d!)")
-	(sequence "|" "CANCELED(c@)"))
+	 "DONE(d!)" "CANCELED(c@)"))
       org-tag-alist '((:startgroup . nil)
 		      ("@admin" . ?a)
 		      ("@call" . ?c)
@@ -38,17 +35,17 @@
 		      ("@errand" . ?e)
 		      ("@message" . ?m)
 		      (:endgroup . nil))
+      org-agenda-files '("~/org/todo.org")
       org-agenda-custom-commands
-      '(("n" "Next tasks" todo "NEXT")))
+      '(("n" "Next tasks" todo "NEXT"))
+      org-capture-templates
+      '(("t" "Task" entry (file+headline "~/org/todo.org" "Tasks")
+	 "* TODO %? %^g\n" :prepend t)
+	("n" "Note" entry (file "~/org/notes.org")
+	 "* %? %^g\n%U\n" :prepend t)))
 
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
-
-(setq org-capture-templates
-      '(("t" "Task" entry (file+headline "~/org/todo.org" "Tasks")
-	 "* TODO %?\n" :prepend t)
-	("n" "Note" entry (file "~/org/notes.org")
-	 "* %? %^g\n%U\n" :prepend t)))
 
 (add-hook 'org-mode-hook 'auto-fill-mode)
 (add-hook 'org-mode-hook 'flyspell-mode)
